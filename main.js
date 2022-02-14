760b32cecefbfa63959c134434a1bb30b04026a6
BetaSoftCologne <info@ollerich.de>
2022-02-14 20:37:43 +0100

    heavily optimized code

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 // Das Element auf dem gemalt werden wird
-var pic = document.getElementById("pic"); 
-var ctx = pic.getContext("2d");
+const pic = document.getElementById("pic"); 
+const ctx = pic.getContext("2d");
+const mid = 60, amp = mid;
 
 // Synthetisierung der CPC-Farbpalette
-let colors = [];
-let x = 255/2.0;
+const colors = [];
+const x = 255/2.0;
 for (let g=0;g<3;g++) {
   for (let r=0;r<3;r++) {
     for (let b=0;b<3;b++) {
-      col = `rgb(${x*r},${x*g},${x*b})`;
+      let col = `rgb(${x*r},${x*g},${x*b})`;
       colors.push(col);
     }
   }
@@ -23,32 +24,29 @@ function draw(c, h) {
   }
 }     
 
-const mid = 60, amp = mid;
-
 // Malung eines Barren
 function bar(target, cols, c, f) {
-  var t = [], x = 0;
-  for (var y of cols) {
-    //t.push(x);
-    //t.push(x);t.push(x);
-    //t.push(x); t.push(x); t.push(y);
-    t.push(x); t.push(x); t.push(y); t.push(x);
+  let t = [], x = 0, y;
+  let push = (t,x,y) => { 
+    t.push(x); t.push(x); t.push(y); t.push(x); 
+  };
+  for (y of cols) {
+    push(t,x,y);
     x = y;
   }
-  t.push(x); t.push(x); t.push(0); t.push(x);
-  cols = t;
+  push(t,x,y);
   y = parseInt(mid + f * amp * Math.sin(c.x));
   c.x += c.y;
-  for (var x of cols) {
+  for (let x of t) {
     target[y++] = x;
   }
 }
 
 // Initialisierung der Barrenschwingungen
 const nummer_der_barren = 9, ay = 0.0464, dx = 1.5 / nummer_der_barren;
-var ax = 1, bars = [];
-for (i=0;i<nummer_der_barren;i++) {
-  bars.push({
+let ax = 1, barren = [];
+for (let i=0;i<nummer_der_barren;i++) {
+  barren.push({
     x: ax, y: ay
   });
   ax += dx;
@@ -66,10 +64,10 @@ const cm = cols.length;
 function frame() {
   // Hintergründiges Muster malen
   const coltab = [];
-  for (i=0;i<200;i++) coltab[i] = 0 + i % 2;
+  for (let i=0;i<200;i++) coltab[i] = 0 + i % 2;
   // Die Barren im Hintergrund einzeichnen
-  for (i=0;i<bars.length;i++) {
-    bar(coltab, cols[i % cm], bars[i], 1.0);
+  for (let i=0;i<barren.length;i++) {
+    bar(coltab, cols[i % cm], barren[i], 1.0);
   }
   // Malung delegieren
   draw(coltab, 1);
