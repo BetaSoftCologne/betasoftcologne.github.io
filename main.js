73b11bab7f083cc2595f8586e07a092ad9781333
BSC <bsc@cpcscene.net>
2024-06-22 17:55:34 +0200

    CPC font, some other stuff

--- main.js
+++ main.js
@@ -1,79 +1,83 @@
 // Das Element auf dem gemalt werden wird
-const pic = document.getElementById("pic"); 
-const ctx = pic.getContext("2d");
-const mid = 60, amp = mid;
+const bild = document.getElementById("pic"); 
+const kontext = bild.getContext("2d");
+const mitte = 60, ablenkung = mitte;
+const zufall = Math.random();
 
 // Synthetisierung der CPC-Farbpalette
-const colors = [];
+const farben = [];
 const x = 255/2.0;
 for (let g=0;g<3;g++) {
   for (let r=0;r<3;r++) {
     for (let b=0;b<3;b++) {
-      let col = `rgb(${x*r},${x*g},${x*b})`;
-      colors.push(col);
+      let farbe = `rgb(${x*r},${x*g},${x*b})`;
+      farben.push(farbe);
     }
   }
 }
 
 // Malen des Farbzwischenspeichers
-function draw(c, h) {
+function male_zwischenspeicher(c, h) {
   for (let i=0; i<c.length;i++) {
-    let y = h*i; // + r + r * Math.sin(ii);
-    ctx.fillStyle = colors[c[i]];
-    ctx.fillRect(0,y,1024,y+h);
+    let y = h*i; 
+    kontext.fillStyle = farben[c[i]];
+    kontext.fillRect(0,y,1024,y+h);
   }
 }     
 
 // Malung eines Barren
-function bar(target, cols, c, f) {
+function male_barren(zielbereich, farben, barren, f) {
   let t = [], x = 0, y;
   let push = (t,x,y) => { 
     t.push(x); t.push(x); t.push(y); t.push(x); 
   };
-  for (y of cols) {
+  for (y of farben) {
     push(t,x,y);
     x = y;
   }
   push(t,x,y);
-  y = parseInt(mid + f * amp * Math.sin(c.x));
-  c.x += c.y;
+  y = parseInt(mitte + f * ablenkung * Math.sin(barren.x));
+  barren.x += barren.y;
   for (let x of t) {
-    target[y++] = x;
+    zielbereich[y++] = x;
   }
 }
 
 // Initialisierung der Barrenschwingungen
-const nummer_der_barren = 9, ay = 0.0464, dx = 1.5 / nummer_der_barren;
-let ax = 1, barren = [];
+const cpc_halbe = 0.0464 / 2;
+const nummer_der_barren = 5 + 3*zufall, 
+  schwingtempo = 0.05 + 0.02*zufall; 
+  dx = 0.4 + 0.3*zufall; 
+let ax = 1, schwingende_barren = [];
 for (let i=0;i<nummer_der_barren;i++) {
-  barren.push({
-    x: ax, y: ay
+  schwingende_barren.push({
+    x: ax, y: schwingtempo
   });
   ax += dx;
 }
 
 // Herstellung der Barrenfärbung
-const cols = [
-  [3,6,15,24,25,24,15,6,3],
-  [9,18,19,22,25,22,19,18,9], 
+const farbige_balken = [
+  [3,6,15,24,15,6,3],
+  [9,18,19,22,19,18,9], 
   [1,2,11,14,20,14,11,2,1]
 ];
-const cm = cols.length;
+const cm = farbige_balken.length;
 
 // Rendierung eins Bildschirms
-function frame() {
+function rendierung(ymax) {
   // Hintergründiges Muster malen
-  const coltab = [];
-  for (let i=0;i<200;i++) coltab[i] = 0 + i % 2;
+  const farbtabelle = [];
+  for (let i=0;i<ymax;i++) farbtabelle[i] = 0 + i % 2;
   // Die Barren im Hintergrund einzeichnen
-  for (let i=0;i<barren.length;i++) {
-    bar(coltab, cols[i % cm], barren[i], 1.0);
+  for (let i=0;i<schwingende_barren.length;i++) {
+    male_barren(farbtabelle, farbige_balken[i % cm], schwingende_barren[i], 1.0);
   }
   // Malung delegieren
-  draw(coltab, 1);
+  male_zwischenspeicher(farbtabelle, 1);
 }
 
 // Starten der Animation
 window.setInterval(function() {
-  frame()
+  rendierung(200);
 }, 25);
